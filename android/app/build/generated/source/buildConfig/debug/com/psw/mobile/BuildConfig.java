9504176ffe11b80bf5a2dae95849e468f1f838af
swoop75 <laoan.ada@outlook.com>
2025-07-26 22:11:46 +0200

    Perfect! I can see the issue. You need to add JitPack repository to your settings.gradle file. The MPAndroidChart   library is hosted on JitPack, not on Google or Maven Central.
     Perfect! I can see the issue. You need to add JitPack repository to your settings.gradle file. The MPAndroidChart
      library is hosted on JitPack, not on Google or Maven Central.

---
+++ android/app/build/generated/source/buildConfig/debug/com/psw/mobile/BuildConfig.java
@@ -0,0 +1,16 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.psw.mobile;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.psw.mobile.debug";
+  public static final String BUILD_TYPE = "debug";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+  // Field from build type: debug
+  public static final String API_BASE_URL = "http://10.0.2.2:3000/api/v1/";
+  // Field from default config.
+  public static final String API_TIMEOUT = "30";
+}
